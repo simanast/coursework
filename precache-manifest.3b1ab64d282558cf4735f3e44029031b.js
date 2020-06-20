0f264f5da174c822d2f2b5fbaecd5d06a6ea931e
simanast <sim.nastya99@gmail.com>
2020-06-20 18:27:28 +1000

    Updates

---
+++ precache-manifest.3b1ab64d282558cf4735f3e44029031b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc36b05822f360ee42dbf0b061290649",
+    "url": "/coursework/index.html"
+  },
+  {
+    "revision": "e78a5e6ed7bf2f36a400",
+    "url": "/coursework/static/css/main.3b052327.chunk.css"
+  },
+  {
+    "revision": "04ddeaf7266687d893a6",
+    "url": "/coursework/static/js/2.78051b9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coursework/static/js/2.78051b9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e78a5e6ed7bf2f36a400",
+    "url": "/coursework/static/js/main.3f36abad.chunk.js"
+  },
+  {
+    "revision": "261c85b77a4d1ed3048f",
+    "url": "/coursework/static/js/runtime-main.268f7519.js"
+  }
+]);
